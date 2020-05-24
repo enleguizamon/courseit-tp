a3b7279771e66ac054b8dcf8ff77885d7a948d84
Eli <eliana.leguizamon@gmail.com>
2020-05-24 16:34:26 -0300

    added carrousel library

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.series').slick({
+        slidesToShow: 3.5,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+    });
+  });
