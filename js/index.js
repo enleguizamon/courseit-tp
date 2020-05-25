aeca3b5450a63aab59d5d953f663d80b59aedd43
Eli <eliana.leguizamon@gmail.com>
2020-05-24 21:00:04 -0300

    added responsive to carrousel and remove unnecessary files

--- js/index.js
+++ js/index.js
@@ -4,5 +4,30 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2.5,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
   });
