833a663f7253f85ac66d7a6695df55e4eebdd29c
Eli <eliana.leguizamon@gmail.com>
2020-05-29 22:33:21 -0300

    added changes to responsive sizes

--- js/index.js
+++ js/index.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
     $('.series').slick({
-        slidesToShow: 3.5,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 2000,
         responsive: [
           {
-            breakpoint: 1124,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-            {
               breakpoint: 800,
               settings: {
                 slidesToShow: 2,
