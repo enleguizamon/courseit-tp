49701678f657cc40c005ad663b3e9d5c04a057a1
Eli <eliana.leguizamon@gmail.com>
2021-09-09 17:59:09 -0300

    pr test

--- js/index.js
+++ js/index.js
@@ -1,56 +1,57 @@
-$(document).ready(function(){
-    $('.series').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        variableWidth: true,
-        autoplaySpeed: 2000,
-        responsive: [
-          {
-              breakpoint: 800,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1.5,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-        ]
-    });
+//esta es una prueba de PR
+
+$(document).ready(function () {
+  $(".series").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    variableWidth: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
+});
 
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.onclick = function(e) {
+navBarToggle.onclick = function (e) {
   e.stopPropagation();
-  toggleMenu()
+  toggleMenu();
 };
 
-mainNav.onclick= function (e) {
+mainNav.onclick = function (e) {
   e.stopPropagation();
-}
+};
 
 document.body.onclick = function () {
   mainNav.classList.remove("active");
-}
+};
 
-
-function toggleMenu () {
-  console.log("onclik funciona")
+function toggleMenu() {
+  console.log("onclik funciona");
   mainNav.classList.toggle("active");
 }
